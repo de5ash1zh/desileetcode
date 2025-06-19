91315215f75684ff35c0c9f9a4f66db27f787fff
Devashish <devashish@Devashishs-MacBook-Air.local>
2025-06-19 11:24:25 +0530

    basic auth routes and controllers init

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import authRoutes from "./routes/auth.routes.js";
 
 const port = process.env.PORT;
 
@@ -11,6 +12,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send(`Welcome to DESI-LEETCODE`);
 });
+
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
